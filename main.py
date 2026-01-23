bceed27e35db72c05c2b70d8f9e7db2ac1abe1bc
Jezura <169179776+Jezura12867@users.noreply.github.com>
2026-01-23 20:03:00 +0100

    COLLISION FINALLY
    I FINALLY added proper not buggy colision. It only took 2 years

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ SCREEN_HEIGHT: int = 900
 
 # Pygame variables
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen: pygame.Surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-icon_sprite = pygame.image.load("Assets/Icon.png")
+icon_sprite: pygame.Surface = pygame.image.load("Assets/Icon.png")
 icon_sprite.set_colorkey("White")
-icon = pygame.display.set_icon(icon_sprite)
-title = pygame.display.set_caption("Rougeu liek")
+icon: None = pygame.display.set_icon(icon_sprite)
+title: None = pygame.display.set_caption("Rougeu liek")
 
 # Sprites
 player_hitbox = pygame.rect.Rect((SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 64, 64))
@@ -37,10 +37,11 @@ class Game:
         
         # NOTE_ TO SELF: CHANGE THESE NUMBERS
         # Decides, what room type you begin in
-        map_num = randint(0, 0)
+        map_num: int = 1
         
-        bullet_spawned_and_dir = [False, 0]
-        bullet_dir = 1
+        bullet_spawned_and_dir: list = [False, 0]
+        bullet_dir: int = 1
+        prev_pos: list = [0, 0]
 
 
         # Sets dodge cooldown
@@ -54,11 +55,17 @@ class Game:
 
 
             # Functions
-            colliding_walls_player = Walls.show(screen, map_num, player_hitbox)
-            in_a_dodge_roll = Player.movement(player_hitbox, SCREEN_WIDTH, SCREEN_HEIGHT, dodge_roll_cooldown, colliding_walls_player)
+            colliding_walls_player: bool = Walls.show(screen, map_num, player_hitbox)
+            if colliding_walls_player == False:
+                prev_pos = [player_hitbox.x, player_hitbox.y]
+
+            player_movement_vars: bool = Player.movement(player_hitbox, SCREEN_WIDTH, SCREEN_HEIGHT, dodge_roll_cooldown, colliding_walls_player, prev_pos, screen, map_num)
             bullet_spawned_and_dir = GunSystem().run(gun, player_hitbox, bullet, SCREEN_WIDTH, SCREEN_HEIGHT, bullet_spawned_and_dir[0], bullet_dir)
 
             # Var updatring
+            in_a_dodge_roll = player_movement_vars[0]
+            if colliding_walls_player != True:
+                prev_pos = player_movement_vars[1]
             bullet_dir = bullet_spawned_and_dir[1]
             
             # Dodge management
--- sprites.py
+++ sprites.py
@@ -2,64 +2,7 @@
 import pygame
 import math
 
-
-class Player:
-    
-    def movement(player_hitbox, SCREEN_WIDTH, SCREEN_HEIGHT, dodge_roll_cooldown, wall_colide) -> bool:
-    
-        # Variables
-        player_movement_speed: int = 5
-        direction:list = [0, 0]
-        
-        
-        # Key input
-        key = pygame.key.get_pressed()
-
-
-        # Movement
-        if key[pygame.K_w] == True:
-            player_hitbox.y -= 1 * player_movement_speed
-            direction[1] = -1
-        
-        if key[pygame.K_s] == True:
-            player_hitbox.y += 1 * player_movement_speed
-            direction[1] = 1
-
-        if key[pygame.K_a] == True:
-            player_hitbox.x -= 1 * player_movement_speed
-            direction[0] = -1
-
-        if key[pygame.K_d] == True:
-            player_hitbox.x += 1 * player_movement_speed
-            direction[0] = 1
-
-        # Collision
-        if wall_colide == True:
-            for i in range(math.ceil(abs(direction[0] * player_movement_speed))):
-                if wall_colide == True:
-                    player_hitbox.x += direction[0] * player_movement_speed / abs(direction[0] * player_movement_speed) * -1
-        
-        if wall_colide == True:    
-            for i in range(math.floor(abs(direction[1] * player_movement_speed))):
-                if wall_colide == True:
-                    player_hitbox.y += direction[1] * player_movement_speed / abs(direction[1] * player_movement_speed) * -1
-
-
-        # Dodge roll
-        dodge_roll_cooldown -= 1
-
-        if dodge_roll_cooldown <= 0:
-            if key[pygame.K_SPACE] == True:
-                player_hitbox.x += direction[0] * player_movement_speed * 50
-                player_hitbox.y += direction[1] * player_movement_speed * 50
-                return True
-            else:
-                return False
-        else:
-            return False
-        
-
-
+from pygame.key import ScancodeWrapper
 
 class Cube(pygame.sprite.Sprite):
 
@@ -70,5 +13,67 @@ class Cube(pygame.sprite.Sprite):
         self.image = pygame.Surface((size, size))
         self.image.fill("red")
         self.rect = self.image.get_rect(topleft = pozice)
+        
+from level import Walls
+
+
+class Player:
+    
+    
+    def movement(player_hitbox, SCREEN_WIDTH, SCREEN_HEIGHT, dodge_roll_cooldown, wall_colide, prev_pos, screen, map_num) -> bool:
+        
+        # Variables
+        player_movement_speed: int = 5
+        direction:list = [0, 0]
+        prev_x = player_hitbox.x
+        prev_y = player_hitbox.y
+        
+        
+        # Key input
+        key: ScancodeWrapper = pygame.key.get_pressed()
+
+
+        # Movement
+        if key[pygame.K_w] == True:
+            direction[1] = -1
+        
+        if key[pygame.K_s] == True:
+            direction[1] = 1
+
+        if key[pygame.K_a] == True:
+            direction[0] = -1
+
+        if key[pygame.K_d] == True:
+            direction[0] = 1     
+
+        
+        player_hitbox.x += direction[0] * player_movement_speed
+        wall_colide: bool = Walls.show(screen, map_num, player_hitbox)
+        if wall_colide == True:
+            player_hitbox.x = prev_pos[0]
+        player_hitbox.y += direction[1] * player_movement_speed
+
+        wall_colide: bool = Walls.show(screen, map_num, player_hitbox)
+        if wall_colide == True:
+            player_hitbox.y = prev_pos[1]
+
+        
+
+
+        # Dodge roll
+        dodge_roll_cooldown -= 1
+
+        if dodge_roll_cooldown <= 0:
+            if key[pygame.K_SPACE] == True:
+                player_hitbox.x += direction[0] * player_movement_speed * 50
+                player_hitbox.y += direction[1] * player_movement_speed * 50
+                return True, prev_pos
+        
+        return False, prev_pos
+
+        
+
+
+
 
     
